c1b1999605ab6a3fff535ae6a01980788af201ba
Reuel Pornobe <riweriweri@gmail.com>
2026-03-28 18:26:56 +0800

    feat(ui): add goal manager

--- awesomeProjectReuelChristian/App.js
+++ awesomeProjectReuelChristian/App.js
@@ -1,39 +1,113 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import {useState} from 'react';
 
 export default function App() {
+
+  const [goalText, setGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText){
+    setGoalText(enteredText)
+  };
+  function addGoalHandler(){
+    setCourseGoals((currentCourseGoals) =>[
+      ...currentCourseGoals,
+      goalText,
+    ]);
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        Group 1
-      </Text>
-      <Text style={styles.subtitle}>
-        AERO KENT DE GUZMAN{"\n"}
-        CYRUZ PETER CABILAN{"\n"}
-        FREDERICK MASANGKAY{"\n"}
-        KYLE FRANZ RODRIGUEZ{"\n"}
-        REUEL CHRISTIAN PORNOBE
-      </Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput}
+        placeholder='Your Course Goal' 
+        placeholderTextColor='#999'
+        onChangeText={goalInputHandler}
+        />
+        
+        <View style={styles.button}>
+          <Button 
+          color = '#4f9667'
+          title='Add Goal'
+          onPress={addGoalHandler}
+          />
+        </View>
+        
+      </View>
+      
+      <View style={styles.goalsContainer}>
+        {courseGoals.map((goal, key) => (
+          <View key={key} style={styles.goalItem}>
+            <Text style={styles.goalText} key={key}>{goal}</Text>
+          </View>
+          ))}
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+
+  appContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    color: '#2d5030',
+
+  textInput: {
+    width: '70%',
+    marginRight: 8,
+    padding: 13,
+    borderWidth: 2,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    fontSize: 16,
   },
-  subtitle: {
-    fontSize: 18,
-    color: '#4f9667',
-    marginTop: 4,
+
+  inputContainer: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 24,
+    borderBottomWidth: 2,
+    borderBottomColor: '#ccc',
   },
+
+  goalsContainer: {
+    width: '100%',
+    flex: 5,
+  },
+
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#4f9667',
+    borderRadius: 10,
+    overflow: 'hidden',
+    height: 45,
+  },
+
+  goalItem: {
+    width: '100%',
+    padding: 12,
+    marginVertical: 6,
+    backgroundColor: '#f0f8f3',
+    borderWidth: 1,
+    borderColor: '#4f9667',
+    borderRadius: 8,
+  },
+
+  goalText: {
+    fontSize: 16,
+    color: '#333',
+    alignItems: 'flex-end',
+  },
+
 });
